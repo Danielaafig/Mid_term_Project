1434ca5fdfa1c0aef13f6539bf9d20ad2814b4f3
sariiimmmm <sarinamasoumi@gmail.com>
2022-11-16 11:25:38 +0100

    data for delivery vs estimate and csv file the code

---
+++ Mysql code/column_selection_2.sql
@@ -0,0 +1,32 @@
+use olist;
+
+select order_id, count(order_id) from order_payments
+group by order_id
+order by count(order_id) desc;
+
+select * from order_payments;
+
+select * from order_payments
+where order_id='fa65dad1b0e818e3ccc5cb0e39231352'
+order by payment_sequential ;
+
+select order_id, round(sum(payment_value),2) as final_payment from order_payments
+group by order_id;
+
+
+
+select customer_id, avg( order_estimated_delivery_date )
+        as average_delivery_date
+  from orders
+  group by customer_id;
+  
+  select order_id,customer_id,
+    order_delivered_customer_date,
+	order_estimated_delivery_date, 
+    datediff(order_estimated_delivery_date, order_delivered_customer_date)as dif_del_est 
+    from orders
+    where order_delivered_customer_date is not null;
+    
+    
+    select customer_id,order_delivered_customer_date from orders
+    where order_delivered_customer_date is not null;
