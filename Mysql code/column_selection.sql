d328c8e9f95b46b89ffd703b389d410b4915c3ae
sariiimmmm <sarinamasoumi@gmail.com>
2022-11-17 16:29:13 +0100

    my sql documentation added

--- Mysql code/column_selection.sql
+++ Mysql code/column_selection.sql
@@ -1,37 +1,59 @@
 use olist;
+
+#checking collecting alll the payment for same order_id 
 select order_id,sum(payment_value) from order_payments
-group by order_id 
-;
+group by order_id ;
+
+
+#checking how many different states we have in the table
 select geolocation_state,count(geolocation_state) from geolocation
 group by geolocation_state;
 
+#selecting and extracting specific columns from the table 
 select order_id, customer_id from orders;
 
-
+#selecting and extracting specific columns from the table 
 select mql_id,business_segment,business_type,seller_id,declared_monthly_revenue from closed_deals;
 
+
+#selecting and extracting specific columns from the table 
 select customer_id , customer_city, customer_state, customer_zip_code_prefix from customers;
 
+#selecting and extracting all columns from the table 
 select * from geolocation;
 select geolocation_state, avg(geolocation_lat), avg(geolocation_lng) from geolocation
 group by geolocation_state;
 
+
+#selecting and extracting specific columns from the table 
 select mql_id, origin from marketing_qualified_leads;
 
+#selecting and extracting specific columns from the table 
 select order_id, customer_city, customer_id,price,freight_value, final_price  from order_customer_items_paid;
 
+#selecting and extracting specific columns from the table 
 select order_id, product_id, seller_id, price, freight_value from order_items;
 
+
+#selecting and extracting specific columns from the table 
 select order_id, payment_value from order_payments;
 
+
+#selecting and extracting specific columns from the table 
 select order_id, review_score from order_reviews;
 
+
+#selecting and extracting specific columns from the table 
 select order_id, year, product_id, category, total_price from order_status_year_price;
 
 
+#selecting and extracting specific columns from the table 
 select product_id, cat, price from prod_cat_price;
 
+
+
+#selecting and extracting all columns from the table 
 select * from sellers;
 
-
+#selecting and extracting all columns from the table 
 select * from orders;
--- Mysql code/column_selection_2.sql
+++ Mysql code/column_selection_2.sql
@@ -1,25 +1,33 @@
 use olist;
 
+#selecting and extracting specific columns from the table 
 select order_id, count(order_id) from order_payments
 group by order_id
 order by count(order_id) desc;
 
+
+#checking the table where there is same order_id have different payment_sequential
 select * from order_payments;
 
 select * from order_payments
 where order_id='fa65dad1b0e818e3ccc5cb0e39231352'
 order by payment_sequential ;
 
+
+
+#sum up the payment value per order_id
 select order_id, round(sum(payment_value),2) as final_payment from order_payments
 group by order_id;
 
 
-
+#extracting the average estimated delivery date ( which didn't give us number of days)
 select customer_id, avg( order_estimated_delivery_date )
         as average_delivery_date
   from orders
   group by customer_id;
-  
+ 
+ 
+ #extracting  the different days between estimated delivery date and actual date 
   select order_id,customer_id,
     order_delivered_customer_date,
 	order_estimated_delivery_date, 
@@ -28,7 +36,8 @@ select customer_id, avg( order_estimated_delivery_date )
     where order_delivered_customer_date is not null
     order by dif_del_est desc;
     
-    
+ 
+ #extracting amount of days that took to deliver actually and estimated
 select order_id,customer_id,
     order_delivered_customer_date,
 	order_estimated_delivery_date, 
@@ -40,25 +49,25 @@ select order_id,customer_id,
     
     
     
-    
+     #checkig how many order_id is per customer is and it was unique 
     select customer_id, count(order_id) from orders
     group by customer_id
     order by count(order_id) desc;
     
+   
     
-    select * from order_reviews;
-    select * from orders;
-    
-    
+   #checking the difference prices between total_price and pyment_value to solve the null value  
     select sum(total_price) from order_status_year_price;
     select sum(payment_value) from order_payments;
    select * from order_payment;
    
+
     select * from order_payments;
-    
+    # eliminate the rows that have canceled their order
     select sum(total_price) from order_status_year_price
     where order_status not like "canceled";
-    
+   
+   #tring to join the the two table but wasn't successfull
     select * from order_payments 
     join order_status_year_price on order_id=order_id; 
     
